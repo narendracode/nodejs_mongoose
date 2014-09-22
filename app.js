85882718cfd23848344baf7f4dad43a580a11e24
Narendra Kumar Soni <narendrasoni2@gmail.com>
2014-09-22 17:32:19 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,48 @@
+var express = require("express");
+var app = express();
+var mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost:27017/foobar");
+var schema = new mongoose.Schema({
+   title: String,
+   body: String,
+   date: {
+      type: Date,
+      default: Date.now
+   }
+});
+
+var BlogPost = mongoose.model("BlogPost",schema);
+
+app.get("/",function(req,res){
+   for(i=0;i<3;i++){
+  var  blogPost = new BlogPost({
+     title: "this is my new title"+i,
+     body : "This is my new body"+i
+  });
+   
+   blogPost.save(function(err){
+      if(err){
+         console.log("Error occured while saving:"+err);
+      }
+   });
+   }
+   res.send("Hello World");
+});
+
+
+app.get("/get",function(req,res){
+   BlogPost.find({},function(err,blogs){
+        if(err){
+           console.log("Error occured while fetching");
+        }else{
+           res.json(blogs);
+        }
+   });
+});
+
+
+
+
+var server = app.listen(3000,function(){
+   console.log("listening on port %d",server.address().port);
+});
